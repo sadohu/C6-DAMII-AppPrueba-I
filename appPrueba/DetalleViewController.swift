e1bf55d4a4654338b2f5e01474477448300bb725
sadohu <sadohu26@yahoo.com>
2023-09-22 20:55:55 -0500

    Add: Acceso a atributos de otra pantalla, dimos de la nueva pantalla

--- appPrueba/DetalleViewController.swift
+++ appPrueba/DetalleViewController.swift
@@ -12,14 +12,18 @@ class DetalleViewController: UIViewController {
     @IBOutlet weak var lblNumero1: UILabel!
     @IBOutlet weak var lblNumero2: UILabel!
     @IBOutlet weak var lblResult: UILabel!
+    /* Variables */
+    var n1 = 0, n2 = 0, result = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        lblNumero1.text = "Número 1: " + String(n1)
+        lblNumero2.text = "Número 2: " + String(n2)
+        lblResult.text = "Resultado: " + String(result)
     }
     
     @IBAction func btnVolver(_ sender: Any) {
+        dismiss(animated: true)
     }
 
 }
--- appPrueba/ViewController.swift
+++ appPrueba/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtNumero2: UITextField!
     @IBOutlet weak var btnCalcular: UIButton!
     @IBOutlet weak var txtResult: UITextField!
+    // OPTIONAL: var num1, num2: String?
+    /* Variables */
+    var n1 = 0, n2 = 0, result = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +25,9 @@ class ViewController: UIViewController {
     
 
     @IBAction func btnCalcular(_ sender: Any) {
-        /* Variables */
-        var n1, n2, result: Int;
         // OPTIONAL: var num1, num2: String?
-        
+        /* Variables */
+        // var n1 = 0, n2 = 0, result = 0
         /* Entrada */
         // OPTIONAL: num1 = txtNumero1.text
         n1 = Int(txtNumero1.text ?? "0") ?? 0;
@@ -39,6 +41,10 @@ class ViewController: UIViewController {
         let data = UIStoryboard(name: "Main", bundle: nil)
         // Acceder al identificador "resume"
         let pantallaResume = data.instantiateViewController(withIdentifier: "resume") as! DetalleViewController
+        // Acceder a los atributos de "resume"
+        pantallaResume.n1 = n1
+        pantallaResume.n2 = n2
+        pantallaResume.result = result
         
         // Direccionar
         present(pantallaResume, animated: true)
