599cf3d9f009059d9607be0681a33d26b47ad8f4
sadohu <sadohu26@yahoo.com>
2023-09-22 22:05:28 -0500

    Add: Fotos y Logica de eventos

---
+++ appPrueba/PersonaViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PersonaViewController.swift
+//  appPrueba
+//
+//  Created by Sadohu on 22/09/23.
+//
+
+import UIKit
+
+class PersonaViewController: UIViewController {
+
+    @IBOutlet weak var btnPersona1: UIButton!
+    @IBOutlet weak var btnPersona2: UIButton!
+    @IBOutlet weak var btnPersona3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnPersona1.setImage(UIImage(named: "radio_off"), for: .normal);
+        btnPersona2.setImage(UIImage(named: "radio_off"), for: .normal);
+        btnPersona3.setImage(UIImage(named: "radio_off"), for: .normal);
+        
+        btnPersona1.setImage(UIImage(named: "radio_on"), for: .selected);
+        btnPersona2.setImage(UIImage(named: "radio_on"), for: .selected);
+        btnPersona3.setImage(UIImage(named: "radio_on"), for: .selected);
+    }
+    @IBAction func btnPersona(_ sender: UIButton) {
+        // Validar el valor de sender
+        if sender == btnPersona1 {
+            btnPersona1.isSelected = true;
+            btnPersona2.isSelected = false;
+            btnPersona3.isSelected = false;
+        }
+        else if sender == btnPersona2 {
+            btnPersona1.isSelected = false;
+            btnPersona2.isSelected = true;
+            btnPersona3.isSelected = false;
+        }
+        else if sender == btnPersona3 {
+            btnPersona1.isSelected = false;
+            btnPersona2.isSelected = false;
+            btnPersona3.isSelected = true;
+        }
+    }
+}
